0f98847c6b62477eb12d20a3a699bcb56732b077
Leshabanellh-png <PC@DESKTOP-26A9125>
2025-11-19 14:30:10 +0200

    Added Timeslot class.

---
+++ Models/Timeslot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie1.Models
+{
+    public class Timeslot
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Start Time")]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        [Display(Name = "End Time")]
+        public DateTime EndTime { get; set; }
+
+        [StringLength(100)]
+        public string? Description { get; set; }
+    }
+}
