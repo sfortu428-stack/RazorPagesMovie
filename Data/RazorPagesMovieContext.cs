ee9a149bb91fac729f81031ac3b3b1befdfb9f2b
fortunate mothiba <sfortu428@gmail.com>
2025-11-20 14:11:56 +0200

    update context

--- Data/RazorPagesMovieContext.cs
+++ Data/RazorPagesMovieContext.cs
@@ -20,7 +20,9 @@ namespace RazorPagesMovie.Data
         public DbSet<RazorPagesMovie.Models.Customers> Customers { get; set; } = default!;
         public DbSet<Admin> Admin { get; set; } = default!;
         public DbSet<RazorPagesMovie.Models.Review> Review { get; set; } = default!;
+        public DbSet<Timeslot>TimeSlot { get; set; } = default!;
+
 
     }
-    
+
 }
